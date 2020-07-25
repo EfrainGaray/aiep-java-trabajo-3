c98f2fc194378695baa647dff114d2acf3d26ce1
Henrry <henrry5dav@outlook.com>
2020-07-25 13:10:19 -0400

    Paquetes con clases
    Clases Producto y Categoría

---
+++ src/main/java/modelo_venta/Producto.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo_venta;
+
+/**
+ *
+ * @author Henrr
+ */
+public class Producto {
+    private int codigo;
+    private String nombre;
+    private int precio;
+    private int stock;
+    private Categoria categoria;
+
+    public Producto(int codigo, String nombre, int precio, int stock, Categoria categoria) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+    
+    public Producto(){}
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+}
